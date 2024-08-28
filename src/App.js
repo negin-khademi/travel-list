ceb86a2aa4e2e892357ba620050293f8055d2bfc
Negin <negin.khademi@gmail.com>
2024-08-28 14:06:58 +0330

    install gh-pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackageList />
+      <Form onAddItems={handleAddItems} />
+      <PackageList items={items} />
       <Stats />
     </div>
   );
@@ -12,19 +19,60 @@ export default function App() {
 function Logo() {
   return <h1>🌴Far Away💼</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { quantity, description, id: Date.now(), packed: false };
+    console.log(newItem);
+    onAddItems(newItem);
+    setDescription('');
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need fot your 😍 trip?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)} />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackageList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
     </div>
   );
 }
-function PackageList() {
-  return <div className="list">List</div>;
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼You have X item on your list, and you already packed X (X%)</em>
     </footer>
   );
